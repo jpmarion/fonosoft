f444d1d04150ce3c09a9bc41fe91fa24fdfc7365
jpmarion <totomarion@gmail.com>
2019-09-09 19:34:19 -0300

    Crear ApiController

---
+++ back/app/Http/Controllers/ApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/* Class ApiController
+ *
+ * @package App\Http\Controllers
+ *
+ * @SWG\Swagger(
+ *      basePath="",
+ *      host="http://fonosoft.local/",
+ *      schemes={"http"},
+ *      @SWG\Info(
+ *          version="1.0",
+ *          title="Fonosoft",
+ *          @SWG\Contact(name="Developer Contact", url="https://www.example.com"),
+ *      )
+ * )
+ */
+
+class ApiController extends Controller
+{
+    //
+}
